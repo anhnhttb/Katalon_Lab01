16c3fedf2829eef9f7d3e4b32dbc3ab8cb0dabca
AnhAnhLiBra <anhnhttb@gmail.com>
2020-10-07 00:04:05 +0700

    Spy Object - Lab07

---
+++ Object Repository/Page_CURA Healthcare Service/i_(678) 813-1KMS_fa fa-envelope-o fa-fw.rs
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>i_(678) 813-1KMS_fa fa-envelope-o fa-fw</name>
+   <tag></tag>
+   <elementGuidId>2004ed81-71e1-4073-93c2-8fea252be85c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>i.fa.fa-envelope-o.fa-fw</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[2]/i</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fa fa-envelope-o fa-fw</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/footer[1]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-lg-10 col-lg-offset-1 text-center&quot;]/ul[@class=&quot;list-unstyled&quot;]/li[2]/i[@class=&quot;fa fa-envelope-o fa-fw&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[2]/i</value>
+   </webElementXpaths>
+</WebElementEntity>
